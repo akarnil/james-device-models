18e7981b1ca04f9d60df00b34bdc06da4b84fc0b
akarnil <138698161+akarnil@users.noreply.github.com>
2023-09-28 10:53:12 +0100

    moved enums + callback func to otademodevice

--- models/otaDemoDevice.py
+++ models/otaDemoDevice.py
@@ -6,6 +6,80 @@ import sys
 sys.path.append("iotconnect")
 from iotconnect import IoTConnectSDK as SdkClient
 
+
+def whoami():
+    import sys
+    return sys._getframe(1).f_code.co_name
+
+from enum import Enum
+
+# 2.1 enums
+class ackCmdStatus(Enum):
+    FAIL = 4,
+    EXECUTED = 5,
+    SUCCESS = 7,
+    EXECUTED_ACK = 6 # what is the difference between this and EXECUTED??
+
+class MessageType(Enum):
+    RPT = 0
+    FLT = 1
+    RPTEDGE = 2
+    RMEdge = 3
+    LOG = 4
+    ACK = 5
+    OTA = 6
+    FIRMWARE = 11
+
+class ErrorCode(Enum):
+    OK = 0
+    DEV_NOT_REG = 1
+    AUTO_REG = 2
+    DEV_NOT_FOUND = 3
+    DEV_INACTIVE = 4
+    OBJ_MOVED = 5
+    CPID_NOT_FOUND = 6
+
+class CommandType(Enum):
+    DCOMM = 0
+    FIRMWARE = 1
+    MODULE = 2
+    U_ATTRIBUTE = 101
+    U_SETTING = 102
+    U_RULE = 103
+    U_DEVICE = 104
+    DATA_FRQ = 105
+    U_barred = 106
+    D_Disabled = 107
+    D_Released = 108
+    STOP = 109
+    Start_Hr_beat = 110
+    Stop_Hr_beat = 111
+    is_connect = 116
+    SYNC = "sync"
+    RESETPWD = "resetpwd"
+    UCART = "updatecrt"
+
+class Option(Enum):
+    attribute = "att"
+    setting = "set"
+    protocol = "p"
+    device = "d"
+    sdkConfig = "sc"
+    rule = "r"
+
+class DataType(Enum):
+    INT = 1
+    LONG = 2
+    FLOAT = 3
+    STRING = 4
+    Time = 5
+    Date = 6
+    DateTime = 7
+    BIT = 8
+    Boolean = 9
+    LatLong = 10
+    OBJECT = 11
+
 class otaDemoDevice(ConnectedDevice):
     api_ver = 2.1
     needs_exit:bool = False
@@ -42,14 +116,26 @@ class otaDemoDevice(ConnectedDevice):
         }
         return data_array
 
-    # def device_cb(self, msg, status=None):
-    #     print("device callback")
-    #     if msg["cmdType"] == self.CMD_TYPE_DEVICE:
-    #         status = self.device_command(msg)
-    #     elif msg["cmdType"] == self.CMD_TYPE_FIRMWARE:
-    #         status = self.firmware_command(msg)
-    #     super().device_cb(msg, status)
+    def device_cb(self,msg):
+        print("device callback received")
+        command_type_got = None
+        if "ct" in msg:
+            if msg["ct"] in CommandType._value2member_map_:
+                command_type_got = CommandType(msg["ct"])
 
+        if command_type_got != None:        
+            child_id_to_send = None
+            if "id" in msg:
+                child_id_to_send = msg["id"]
+
+            if command_type_got == CommandType.DCOMM:
+                self.SdkClient.sendAckCmd(msg["ack"], ackCmdStatus.SUCCESS, "Sending SUCCESSFUL", child_id_to_send)
+            else:
+                print(whoami() + " got sent command_type     " + command_type_got.name)
+            return
+
+        print("callback received not valid")
+        print("rule command",msg)
     def device_command(self, msg):
         command = msg['command'].split(' ')
         if command[0] == "setPower":
