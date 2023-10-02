0bca8e66e1ac71d44ef6836183623b98675aad6c
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-02 09:31:01 +0100

    use nested enums and stuff

--- credential_parser.py
+++ credential_parser.py
@@ -10,38 +10,59 @@ path_to_json = "/home/akarnil/Documents/Work/james-device-models/credentials.jso
 #     "devicePrimaryKey":"aHSkrFGVo3ezFoQLfUx7zQ=="
 # }
 
+# this function and the assignment below allows python to have nested enums, this is nice to model the auth types
+def enum__getattr__(self, item):
+    if item != '_value_':
+        return getattr(self.value, item)
+    raise AttributeError
+Enum.__getattr__ = enum__getattr__
 
-class k(str,Enum):
-    device_id = "duid"
-    company_id = "cpid"
-    environment = "env"
-    auth_type = "auth_type"
-    auth_fields = "auth_fields"
-    sdk_id = "sdk_id"
-
-
-
-class auth_types(Enum):
-    x509 = "x509"
-    symmetric = "symmetric"
-
-class at_symmetric(str,Enum):
-    primary_key = "primary_key"
-
-class at_x509(str,Enum):
-    client_key = "client_key"
-    client_cert = "client_cert"
-
-at2m = {
-    auth_types.x509.value : at_x509,
-    auth_types.symmetric.value : at_symmetric,
-}
+def tV(enum: Enum) -> str:
+    return enum.value
 
 
-def get(json, key):
-    if key in json:
-        return json[key]
+class k(Enum):
+    device_id:str = "duid"
+    company_id:str = "cpid"
+    environment:str = "env"
+    auth:str = "auth"
+    sdk_id:str = "sdk_id"
+
+class auth(Enum):
+    type: str = "type"
+    params: str = "params"
+
+    class x509(Enum):
+        name: str = "x509"
+        class children(Enum):
+            client_key:str = "client_key"
+            client_cert:str = "client_cert"
+
+    class symmetric(Enum):
+        name: str = "symmetric"
+        class children(Enum):
+            primary_key:str = "primary_key"
+        
+
+def get(json, key: Enum):
+    if key.value in json:
+        return json[key.value]
     return None
+
+def parse_auth(j: json):
+    auth_o = get(j, k.auth)
+    auth_type = get(auth_o, auth.type)
+    params_o = get(auth_o, auth.params) # need to fetch child object for get() to work with nested enums
+
+    if auth_type == auth.symmetric.name:
+        primary_key = get(params_o, auth.symmetric.children.primary_key)
+        pass
+
+    if auth_type == auth.x509.name:
+        client_key = get(params_o, auth.x509.children.client_key)
+        client_cert = get(params_o, auth.x509.children.client_cert)
+        pass
+    
 
 if __name__ == "__main__":
 
@@ -60,18 +81,6 @@ if __name__ == "__main__":
     device_id = get(j, k.device_id)
     company_id = get(j, k.company_id)
     environment = get(j, k.environment)
-    auth_type = get(j, k.auth_type)
     sdk_id = get(j, k.sdk_id)
 
-    auth_fields: json = get(j, k.auth_fields)
-
-    if auth_type == auth_types.symmetric.name:
-        primary_key = get(auth_fields, at2m[auth_type].primary_key)
-        pass
-
-    if auth_type == auth_types.x509.name:
-        client_key = get(auth_fields, at2m[auth_type].client_key)
-        client_cert = get(auth_fields, at2m[auth_type].client_cert)
-        pass
-    
-    print(device_id)
+    parse_auth(j)
