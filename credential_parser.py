4014c6f372f8b91d513b405e32add48fde75e917
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-03 10:11:54 +0100

    Linting and documenting credential parser

--- credential_parser.py
+++ credential_parser.py
@@ -1,22 +1,19 @@
+"""Load a device json file from path and translate to format required for the SDK"""
 import json
-from enum import Enum
-
-from models.device_model import ConnectedDevice
-
-path_to_json = "credentials.json"
 
 
-class sdk_options:
-    class certificate:
+class SdkOptions:
+    """Human readable Enum for to mapping SDK's sdkOptions format"""
+    class Certificate:
         name:str = "certificate"
-        class children:
+        class Children:
             key_path:str = "SSLKeyPath"
             cert_path: str = "SSLCertPath"
             root_cert_path: str = "SSLCaPath"
     
-    class offline_storage:
+    class OfflineStorage:
         name:str = "offlineStorage"
-        class children:
+        class Children:
             disabled:str = "disabled"
             available_space: str = "availSpaceInMb"
             file_count: str = "fileCount"
@@ -24,7 +21,8 @@ class sdk_options:
     symmetric_primary_key: str = "devicePrimaryKey"
 
 
-class k:
+class Keys:
+    """Human readable Enum for to mapping credential's json format"""
     unique_id:str = "duid"
     company_id:str = "cpid"
     environment:str = "env"
@@ -33,46 +31,50 @@ class k:
     device:str = "device"
     message_version:str = "message_version"
 
-class auth:
+class Auth:
+    """Human readable Enum for to mapping credential's auth object json format, including subclasses"""
     type: str = "type"
     params: str = "params"
 
-    class x509:
+    class X509:
         name:str = "x509"
-        class children:
+        class Children:
             client_key:str = "client_key"
             client_cert:str = "client_cert"
             root_cert:str = "root_cert"
 
-    class symmetric:
+    class Symmetric:
         name:str = "symmetric"
-        class children:
+        class Children:
             primary_key:str = "primary_key"
         
 
-class device:
-    class offline_storage:
+class Device:
+    """Human readable Enum for to mapping credential's device object json format, including subclasses"""
+    class OfflineStorage:
         name:str = "offline_storage"
-        class children:
+        class Children:
             available_space:str = "available_space_MB"
             file_count:str = "file_count"
 
-def get(json, key):
+def get(j: json, key):
+    """Get value from key, return None if it doesn't exist"""
     if not isinstance(key,str):
         key = key.__name__
-    if key in json:
-        return json[key]
+    if key in j:
+        return j[key]
     return None
 
 def parse_json_for_config(path_to_json) -> dict:
+    """Create and return credentials dictionary with values from json"""
     j = get_json_from_file(path_to_json)
 
     credentials: dict = {}
-    credentials["message_version"] = get(j, k.message_version)
-    credentials["company_id"] = get(j, k.company_id)
-    credentials["unique_id"] = get(j, k.unique_id)
-    credentials["environment"] = get(j, k.environment)
-    credentials["sdk_id"] = get(j, k.sdk_id)
+    credentials["message_version"] = get(j, Keys.message_version)
+    credentials["company_id"] = get(j, Keys.company_id)
+    credentials["unique_id"] = get(j, Keys.unique_id)
+    credentials["environment"] = get(j, Keys.environment)
+    credentials["sdk_id"] = get(j, Keys.sdk_id)
 
     sdk_options: dict[str] = {}
     sdk_options.update(parse_auth(j))
@@ -82,21 +84,24 @@ def parse_json_for_config(path_to_json) -> dict:
     return credentials
 
 def get_and_assign(from_json, to_obj , from_key, to_key):
+    """Get value from json, and assign to object in format needed for the SDK"""
     if (temp := get(from_json, from_key)) is not None:
-            to_obj[to_key] = temp
+        to_obj[to_key] = temp
 
 def parse_device(j: json):
+    """Parse device object from credential json, generate format needed for SDK"""
     temp: dict[str] = {}
-    device_o = get(j, k.device)
+    device_o = get(j, Keys.device)
 
+    # Parse offline_storage parameters
     child: dict[str] = {}
-    if (offline_storage_o := get(device_o, device.offline_storage.name)) is not None:
-        child[sdk_options.offline_storage.children.disabled] = False
-        get_and_assign(offline_storage_o,child, device.offline_storage.children.available_space, sdk_options.offline_storage.children.available_space)
-        get_and_assign(offline_storage_o,child, device.offline_storage.children.file_count, sdk_options.offline_storage.children.file_count)
+    if (offline_storage_o := get(device_o, Device.OfflineStorage.name)) is not None:
+        child[SdkOptions.OfflineStorage.Children.disabled] = False
+        get_and_assign(offline_storage_o,child, Device.OfflineStorage.Children.available_space, SdkOptions.OfflineStorage.Children.available_space)
+        get_and_assign(offline_storage_o,child, Device.OfflineStorage.Children.file_count, SdkOptions.OfflineStorage.Children.file_count)
     else:
-        child[sdk_options.offline_storage.children.disabled] = True
-    temp[sdk_options.offline_storage.name] = child
+        child[SdkOptions.OfflineStorage.Children.disabled] = True
+    temp[SdkOptions.OfflineStorage.name] = child
 
 
 
@@ -105,70 +110,34 @@ def parse_device(j: json):
 
 
 def parse_auth(j: json):
+    """Parse auth object from credential json, generate format needed for SDK"""
     temp: dict[str] = {}
 
-    auth_o = get(j, k.auth)
-    auth_type = get(auth_o, auth.type)
-    params_o = get(auth_o, auth.params) # need to fetch child object for get() to work with nested enums
+    auth_o = get(j, Keys.auth)
+    params_o = get(auth_o, Auth.params)
 
-    if auth_type == auth.symmetric.name:
-    # from jon
-    #
-    #     "auth": {
-    #     "type": "symmetric",
-    #     "params": {
-    #         "primary_key": "a"
-    #     }
-    # },
+    auth_type = get(auth_o, Auth.type)
+    if auth_type == Auth.Symmetric.name:
+        get_and_assign(params_o,temp, Auth.Symmetric.Children.primary_key, SdkOptions.symmetric_primary_key)
 
-    # to sdj
-    #
-    # 	"devicePrimaryKey" : "a"
-
-        get_and_assign(params_o,temp, auth.symmetric.children.primary_key, sdk_options.symmetric_primary_key)
-        
-    elif auth_type == auth.x509.name:
-    # from json
-    
-    #     "auth": {
-    #     "type": "x509",
-    #     "params": {
-    #         "client_key": "a",
-    #         "client_cert": "b",
-    #         "root_cert": "c"
-    #     }
-    # },
-
-    # to sdk
-    #
-    # 	"certificate" : { 
-    # 		"SSLKeyPath"  : "a",
-    # 		"SSLCertPath" : "b",
-    # 		"SSLCaPath"   : "c"    
-    # 	},
-
+    elif auth_type == Auth.X509.name:
         child: dict[str] = {}
-        get_and_assign(params_o,child, auth.x509.children.client_key, sdk_options.certificate.children.key_path)
-        get_and_assign(params_o,child, auth.x509.children.client_cert, sdk_options.certificate.children.cert_path)
-        get_and_assign(params_o,child, auth.x509.children.root_cert, sdk_options.certificate.children.root_cert_path)
-        temp[sdk_options.certificate.name] = child
-        
+        get_and_assign(params_o,child, Auth.X509.Children.client_key, SdkOptions.Certificate.Children.key_path)
+        get_and_assign(params_o,child, Auth.X509.Children.client_cert, SdkOptions.Certificate.Children.cert_path)
+        get_and_assign(params_o,child, Auth.X509.Children.root_cert, SdkOptions.Certificate.Children.root_cert_path)
+        temp[SdkOptions.Certificate.name] = child
+
 
     return temp
 
-
-
-
-
 def get_json_from_file(path):
+    """Load Json from file and return json object"""
     j: json = None
-    with open(path, "r") as f:
-        f_contents = f.read()
+    with open(path, "r", encoding="utf-8") as file:
+        f_contents = file.read()
         j = json.loads(f_contents)
     return j
 
 if __name__ == "__main__":
-    parse_json_for_config(path_to_json)
-
-
-
+    PATH_TO_JSON = "credentials.json"
+    print(parse_json_for_config(PATH_TO_JSON))
